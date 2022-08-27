3ef3cc0ccec854b53b3936b9221d44fd0c3ac475
Alessandra Canuto <ale.canutto@gmail.com>
2022-08-27 09:53:08 -0300

    Schema migration

--- src/prisma/migrations/20220827124648_initial_db/migration.sql
+++ src/prisma/migrations/20220827124648_initial_db/migration.sql
@@ -1,8 +1,8 @@
 -- CreateTable
 CREATE TABLE `users` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `email` VARCHAR(191) NOT NULL,
-    `password` VARCHAR(191) NOT NULL,
+    `email` VARCHAR(60) NOT NULL,
+    `password` VARCHAR(20) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
 
     UNIQUE INDEX `users_email_key`(`email`),
@@ -12,10 +12,10 @@ CREATE TABLE `users` (
 -- CreateTable
 CREATE TABLE `professionals` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `crp` VARCHAR(191) NOT NULL,
-    `cellphone` VARCHAR(191) NULL,
-    `approach` VARCHAR(191) NOT NULL,
+    `name` VARCHAR(150) NOT NULL,
+    `crp` VARCHAR(20) NOT NULL,
+    `cellphone` VARCHAR(14) NULL,
+    `approach` VARCHAR(30) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `userId` INTEGER NOT NULL,
 
@@ -28,34 +28,35 @@ CREATE TABLE `professionals` (
 -- CreateTable
 CREATE TABLE `patients` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `name` VARCHAR(191) NOT NULL,
-    `cpf` VARCHAR(191) NOT NULL,
-    `email` VARCHAR(191) NOT NULL,
-    `gender` VARCHAR(191) NOT NULL,
-    `cellphone` VARCHAR(191) NULL,
+    `name` VARCHAR(150) NOT NULL,
+    `cpf` VARCHAR(14) NOT NULL,
+    `email` VARCHAR(60) NOT NULL,
+    `gender` VARCHAR(15) NOT NULL,
+    `cellphone` VARCHAR(14) NULL,
     `birth_date` DATETIME(3) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
+    `professionalId` INTEGER NOT NULL,
 
     UNIQUE INDEX `patients_cpf_key`(`cpf`),
     UNIQUE INDEX `patients_email_key`(`email`),
+    UNIQUE INDEX `patients_professionalId_key`(`professionalId`),
+    INDEX `professional`(`professionalId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
 -- CreateTable
 CREATE TABLE `sessions` (
     `id` INTEGER NOT NULL AUTO_INCREMENT,
-    `subject` VARCHAR(191) NOT NULL,
-    `type` VARCHAR(191) NOT NULL,
-    `status` VARCHAR(191) NOT NULL,
-    `schedule_type` VARCHAR(191) NOT NULL,
+    `subject` VARCHAR(150) NOT NULL,
+    `type` VARCHAR(30) NOT NULL,
+    `status` VARCHAR(20) NOT NULL,
+    `schedule_type` VARCHAR(150) NOT NULL,
     `duration` INTEGER NOT NULL,
     `session_date` DATETIME(3) NOT NULL,
     `created_at` DATETIME(3) NOT NULL DEFAULT CURRENT_TIMESTAMP(3),
     `professionalId` INTEGER NOT NULL,
-    `patientId` INTEGER NOT NULL,
 
     INDEX `professional`(`professionalId`),
-    INDEX `patient`(`patientId`),
     PRIMARY KEY (`id`)
 ) DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 
@@ -74,10 +75,10 @@ CREATE TABLE `patients_session` (
 ALTER TABLE `professionals` ADD CONSTRAINT `professionals_userId_fkey` FOREIGN KEY (`userId`) REFERENCES `users`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `sessions` ADD CONSTRAINT `sessions_professionalId_fkey` FOREIGN KEY (`professionalId`) REFERENCES `professionals`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `patients` ADD CONSTRAINT `patients_professionalId_fkey` FOREIGN KEY (`professionalId`) REFERENCES `professionals`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
-ALTER TABLE `sessions` ADD CONSTRAINT `sessions_patientId_fkey` FOREIGN KEY (`patientId`) REFERENCES `patients`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
+ALTER TABLE `sessions` ADD CONSTRAINT `sessions_professionalId_fkey` FOREIGN KEY (`professionalId`) REFERENCES `professionals`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
 
 -- AddForeignKey
 ALTER TABLE `patients_session` ADD CONSTRAINT `patients_session_sessionId_fkey` FOREIGN KEY (`sessionId`) REFERENCES `sessions`(`id`) ON DELETE RESTRICT ON UPDATE CASCADE;
